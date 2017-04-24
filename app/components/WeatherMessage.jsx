dd3e0f879ab046b87ab549a1c17103a853071938
Jay Letheby <jay.letheby@gmail.com>
2017-04-24 21:42:43 +1000

    first commit

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+  render: function () {
+    return (
+      <p>The Weather in Peregian beach is Fucking mint</p>
+    );
+  }
+});
+
+module.exports = WeatherMessage;

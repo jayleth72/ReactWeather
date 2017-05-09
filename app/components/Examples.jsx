a07b8ffabe965cb91eb1cd8f87a2dda1b2cb1c99
Jay Letheby <jay.letheby@gmail.com>
2017-05-09 21:42:18 +1000

    styles examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Peregian Beach'>Peregian Beach, QLD</Link>
+        </li>
+        <li>
+          <Link to='/?location=Mclaren Flat'>Mclaren Flat, SA</Link>
+        </li>
+      </ol>
+    </div>
+
   );
 };
 

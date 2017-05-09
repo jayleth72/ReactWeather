732f6d4b48a53be1a8fd42faa1727af56733ab01
Jay Letheby <jay.letheby@gmail.com>
2017-05-09 16:54:13 +1000

    Prepare repo for Heroku

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express = require('express');
 
 // Create app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+// fix hhtp - https for openweathermap
+app.use(function (req, res, next){
+  if(req.beaders['x-forwareded-proto'] === 'http'){
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(port, function () {
+  console.log('Express server is up on port' + PORT);
 });
